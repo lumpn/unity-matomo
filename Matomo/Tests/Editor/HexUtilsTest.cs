b093f4a287486c697b40cf43620e194973c78754
Jonas Boetel <lumpn@users.noreply.github.com>
2021-01-13 16:24:23 +0800

    Fixed assembly definitions

--- Matomo/Tests/Editor/HexUtilsTest.cs
+++ Matomo/Tests/Editor/HexUtilsTest.cs
@@ -1,7 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//----------------------------------------
+// MIT License
+// Copyright(c) 2021 Jonas Boetel
+//---------------------------------------- 
+using System.Text;
 using NUnit.Framework;
+using Lumpn.Matomo.Utils;
 
 namespace Lumpn.Matomo.Test
 {
@@ -10,7 +13,14 @@ namespace Lumpn.Matomo.Test
         [Test]
         public void BytesToHex()
         {
-            var bytes = new byte[] { 0x20, 0x30 };
+            var bytes = new byte[] { 0x0D, 0x15, 0xEA, 0x5E };
+            var expected = "0D15EA5E";
+
+            var builder = new StringBuilder();
+            HexUtils.AppendHex(builder, bytes);
+            var actual = builder.ToString();
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
